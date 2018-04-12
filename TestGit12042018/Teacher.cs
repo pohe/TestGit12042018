7e9726a49d13be8be73e10cb9d3b3d9331ef5525
Poul2017 <po.henriksen@gmail.com>
2018-04-12 14:21:52 +0200

    Teacher added

---
+++ TestGit12042018/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit12042018
+{
+    class Teacher
+    {
+        public string Name { get; set; }
+
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"The teachers name {Name}";
+        }
+    }
+}
